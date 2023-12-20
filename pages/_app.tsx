4725cbb61be23492fd0ced8672abdbbde16f7d76
문소희 <thgmlxn@hanmail.net>
2023-12-20 11:06:47 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,10 @@ function MyApp({ Component }: AppProps): JSX.Element {
         <title>Women Snap</title>
         <meta name="description" content="패션이 쉬워진다. 우먼스냅" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
